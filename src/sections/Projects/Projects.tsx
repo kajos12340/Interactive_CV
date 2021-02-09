400506f547164e11d56c4b57f45d1b2eadcd2395
kajos12340 <piotr.kajka.96@gmail.com>
2021-02-09 16:11:23 +0100

    projects reordered

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -11,18 +11,6 @@ import Button from '../../Common/Button/Button';
 
 const projectsData: SingleProject[] = [
   {
-    description: 'LiftIt is application I\'ve created as my master degree. It\'s purpose is to plan, process, manage and overview powerlifting training, but due to it\'s architecture and solutions, it can be used for any type of training. Client side of this application is Progressive Web App created using TypeScript, Next.JS, Redux, Ant Design and Styled Components. Client side is REST API created with TypeScript, Node.js (Express) and MongoDB. Modules I\'m the most proud of are three stages training plan creation form, which enables to plan every aspect of training, training session which tells everything about current step of training, count reps and rest, statistics module, enabling us to continuously overview our progress and finally JWT Authorization.',
-    img: liftItImg,
-    links: (
-      <>
-        <Button href="https://liftitnow.now.sh/">Website</Button>
-        <Button href="https://github.com/kajos12340/MyEpicWorkout-UI">Github Front-end</Button>
-        <Button href="https://github.com/kajos12340/MyEpicWorkout-API">Github Back-end</Button>
-      </>
-    ),
-    title: 'LiftIT - body building training app',
-  },
-  {
     description: 'I have created this application to make it easier to coordinate shopping with my fiancee. It enables us to add, remove and status products in real time, which makes our shopping much easier. To acquire this purpose application is based on WebSocket technology. Client site was created with TypeScript, React, Material-UI and Redux. Server side is Express REST API and MongoDB database with Mongoose.',
     img: shopImg,
     links: (
@@ -41,6 +29,14 @@ const projectsData: SingleProject[] = [
       <Button href="https://github.com/kajos12340/Interactive_CV">Github</Button>
     ),
     title: 'Personal Website',
+  },
+  {
+    description: 'LiftIt is application I\'ve created as my master degree. It\'s purpose is to plan, process, manage and overview powerlifting training, but due to it\'s architecture and solutions, it can be used for any type of training. Client side of this application is Progressive Web App created using TypeScript, Next.JS, Redux, Ant Design and Styled Components. Client side is REST API created with TypeScript, Node.js (Express) and MongoDB. Modules I\'m the most proud of are three stages training plan creation form, which enables to plan every aspect of training, training session which tells everything about current step of training, count reps and rest, statistics module, enabling us to continuously overview our progress and finally JWT Authorization.',
+    img: liftItImg,
+    links: (
+      <Button href="https://liftitnow.now.sh/">Website</Button>
+    ),
+    title: 'LiftIT - body building training app',
   },
 ];
 
